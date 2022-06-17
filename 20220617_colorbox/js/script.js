11aad2816eeefcdcc15702f4620d308599711b2c
soyeong0411 <2so092@naver.com>
2022-06-17 16:40:03 +0900

    0617

---
+++ 20220617_colorbox/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".modal").colorbox({
+        rel:'modal'
+    })
+    $(".youtube").colorbox({
+        iframe:true,        // 옵션명:value
+        innerWidth:700,
+        innerHeight:394
+    })
+});
