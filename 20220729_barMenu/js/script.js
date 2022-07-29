0aa1292f5084db0c3414a95d35cb51e33beda4b8
soyeong0411 <2so092@naver.com>
2022-07-29 14:26:18 +0900

    0729

--- 20220729_barMenu/js/script.js
+++ 20220729_barMenu/js/script.js
@@ -1,1 +1,19 @@
 console.log("test")
+
+// $(".navbar ul li").mouseover(function(){
+//     let i = $(this).index();
+//     console.log(i);
+
+//     let value = i*240
+//     $(".bar").css("left",value)
+// })
+
+
+$(".navbar ul li").mouseover(over);  //함수가 인자값으로 들어감 =콜백함수
+$(".navbar ul li").mouseout(out);
+
+function over(){
+    let i = $(this).index();
+    let value = i*240;
+    $(".bar").css({left:value})
+}
