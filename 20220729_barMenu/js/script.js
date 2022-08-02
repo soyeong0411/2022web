68787cc65d5f92c847e68b4e59b134178fdff470
soyeong0411 <2so092@naver.com>
2022-08-02 15:18:21 +0900

    0802

--- 20220729_barMenu/js/script.js
+++ 20220729_barMenu/js/script.js
@@ -8,12 +8,35 @@ console.log("test")
 //     $(".bar").css("left",value)
 // })
 
+// menu 0 1 2 3 4
+function nowMenu(menu){
+    console.log(menu)
 
-$(".navbar ul li").mouseover(over);  //함수가 인자값으로 들어감 =콜백함수
-$(".navbar ul li").mouseout(out);
+    if(menu == "main"){
+        $(".navbar ul").find("li").mouseover(over);  //함수가 인자값으로 들어감 =콜백함수
+        $(".navbar ul").mouseout(out);
+    }else{
+        $(".navbar li").eq(menu).addClass("active") // 현재 메뉴의 위치(after에서 점)
+        $(".bar").css({left:menu*240,opacity:1}) // 처음 바의 위치
+
+        $(".navbar ul").find("li").mouseover(over);  //함수가 인자값으로 들어감 =콜백함수
+        $(".navbar ul").mouseout(function(){
+            $(".bar").css({left:menu*240,opacity:1}) // mouseout 시 원위치로 이동
+        });
+    }
+
+
+}
+
+
+
 
 function over(){
     let i = $(this).index();
     let value = i*240;
-    $(".bar").css({left:value})
-}
+    $(".bar").css({left:value,opacity:1})
+    }
+    function out(){
+    
+    $(".bar").css({opacity:0})
+    }   
